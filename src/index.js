193e49afb742d1cfbbc947f098f1fd1dae1a4276
fatima ahmed <fatymah001@gmail.com>
2019-11-01 02:48:04 +0300

    Fix stickler errors

--- src/index.js
+++ src/index.js
@@ -1,51 +1,44 @@
 import * as view from './view';
 
-function getWeather(location, option){
-	if (option ===1){
-		getWeatherInFahrenheit(`http://api.openweathermap.org/data/2.5/weather?q=${document.getElementById('searchBox').value}&APPID=e35c92eefa655c7d2e50f8101fc984ca`);
-	}
-	else {
-		getWeatherInCelcius(`http://api.openweathermap.org/data/2.5/weather?q=${document.getElementById('searchBox').value}&APPID=e35c92eefa655c7d2e50f8101fc984ca`);
-	}
+async function getWeatherInCelcius(url) {
+  try {
+    const response = await fetch(url, { mode: 'cors' });
+    const result = await response.json();
+    const weatherInfo = result.main;
+    const temp = weatherInfo.temp - 273.15;
+    const tempMin = weatherInfo.temp_min - 273.15;
+    const tempMax = weatherInfo.temp_max - 273.15;
+    const { pressure } = weatherInfo;
+    const { humidity } = weatherInfo;
+    view.displayWeatherInfo(temp, tempMin, tempMax, pressure, humidity);
+  } catch (err) {
+    alert('city not found');
+  }
 }
 
-async function getWeatherInCelcius(url)
-{
-    try{
-        const response = await fetch(url, { mode: 'cors'});
-        const result = await response.json();
-				const weatherInfo = result.main;
-				const temp = weatherInfo.temp - 273.15;
-				const tempMin = weatherInfo.temp_min - 273.15;
-				const tempMax = weatherInfo.temp_max - 273.15;
-				const pressure = weatherInfo.pressure;
-				const humidity = weatherInfo.humidity;
-				view.displayWeatherInfo(temp, tempMin , tempMax, pressure, humidity);
-			}	
-		 catch(err) {
-			alert('city not found'); 
-			}
-}	
-		async function getWeatherInFahrenheit(url) 
-    {
-				try{
-					const response = await fetch(url, { mode: 'cors'});
-					const result = await response.json();
-					const weatherInfo = result.main;
-					const temp = (weatherInfo.temp - 273.15) * 9/5 + 32;
-					const tempMin = (weatherInfo.temp_min - 273.15) * 9/5 + 32 ;
-					const tempMax = (weatherInfo.temp_max - 273.15) * 9/5 + 32 ;
-					const pressure = weatherInfo.pressure;
-					const humidity = weatherInfo.humidity;
-					view.displayWeatherInfo(temp, tempMin , tempMax, pressure, humidity);
-				}
-			 catch(err) {
-					alert('city not found'); 
-    	}	
-		}
+async function getWeatherInFahrenheit(url) {
+  try {
+    const response = await fetch(url, { mode: 'cors' });
+    const result = await response.json();
+    const weatherInfo = result.main;
+    const temp = ((weatherInfo.temp - 273.15) * 9) / 5 + 32;
+    const tempMin = ((weatherInfo.temp_min - 273.15) * 9) / 5 + 32;
+    const tempMax = ((weatherInfo.temp_max - 273.15) * 9) / 5 + 32;
+    const { pressure } = weatherInfo;
+    const { humidity } = weatherInfo;
+    view.displayWeatherInfo(temp, tempMin, tempMax, pressure, humidity);
+  } catch (err) {
+    alert('city not found');
+  }
+}
+function getWeather(location, option) {
+  if (option === 1) {
+    getWeatherInFahrenheit(`http://api.openweathermap.org/data/2.5/weather?q=${document.getElementById('searchBox').value}&APPID=e35c92eefa655c7d2e50f8101fc984ca`);
+  } else {
+    getWeatherInCelcius(`http://api.openweathermap.org/data/2.5/weather?q=${document.getElementById('searchBox').value}&APPID=e35c92eefa655c7d2e50f8101fc984ca`);
+  }
+}
 
-	document.onload(view.displaySearchBox());
+document.onload(view.displaySearchBox());
 
-export {
-	getWeather
-};
+export default getWeather;
--- src/view.js
+++ src/view.js
@@ -1,64 +1,62 @@
-import { getWeather } from './index';
+import getWeather from './index';
 
 const getCityName = () => {
-	const city = document.getElementById('searchBox').value;
-	if (document.getElementById('fahrenheitBtn').checked===true){
-		getWeather(city,1);
-	}
-	else {
-		getWeather(city,0);
-	}
-}
+  const city = document.getElementById('searchBox').value;
+  if (document.getElementById('fahrenheitBtn').checked === true) {
+    getWeather(city, 1);
+  } else {
+    getWeather(city, 0);
+  }
+};
 
 const displaySearchBox = () => {
-	const weatherInfo = document.getElementById('weatherInfo');
-	const results = document.createElement('div');
-	results.id = 'results';
-	results.innerHTML = '';
-	weatherInfo.innerHTML = `<h1>My weather app</h1>`;
-	const searchBox = document.createElement('input');
-	const searchBtn = document.createElement('button');
-	const radioBtn = document.createElement('div');
-	const fahrenheitBtn = document.createElement('input');
-	const celciusBtn = document.createElement('input');
-	radioBtn.id = 'radioBtn';
-	fahrenheitBtn.setAttribute('type', 'radio');
-	fahrenheitBtn.id = 'fahrenheitBtn';
-	celciusBtn.setAttribute('type', 'radio');
-	celciusBtn.id = 'celciusBtn';
-	fahrenheitBtn.setAttribute('name', 'tempType');
-	const F = document.createElement('span');
-	F.innerHTML = 'F';
-	const C = document.createElement('span');
-	C.innerHTML = 'C';
-	F.id = 'F';
-	C.id = 'C';
-	celciusBtn.setAttribute('name', 'tempType');
-	searchBox.id = 'searchBox'
-	searchBtn.id = 'searchBtn'
-	searchBox.setAttribute('placeholder', 'city');
-	searchBtn.innerHTML = 'check weather';
-	radioBtn.appendChild(fahrenheitBtn);
-	radioBtn.appendChild(F);
-	radioBtn.appendChild(celciusBtn);
-	radioBtn.appendChild(C);
-	weatherInfo.appendChild(searchBox);
-	weatherInfo.appendChild(radioBtn);
-	weatherInfo.append(searchBtn);
-	weatherInfo.appendChild(results);
-	searchBtn.addEventListener('click', getCityName);
-}
-const displayWeatherInfo= (temp, tempMin , tempMax, pressure, humidity) => {
-	const results = document.getElementById('results');
-	results.innerHTML = `<h2>temperature: ${temp.toFixed(2)}</h2>`;
-	results.innerHTML += `<h3>min temperature: ${tempMin.toFixed(2)}</h3>`; 
-	results.innerHTML += `<h3>max temperature: ${tempMax.toFixed(2)}</h3>`; 
-	results.innerHTML += `<h2>pressure: ${pressure}hPa</h2>`; 
-	results.innerHTML += `<h2>humidity: ${humidity}%</h2>`; 
-
-}
+  const weatherInfo = document.getElementById('weatherInfo');
+  const results = document.createElement('div');
+  results.id = 'results';
+  results.innerHTML = '';
+  weatherInfo.innerHTML = '<h1>My weather app</h1>';
+  const searchBox = document.createElement('input');
+  const searchBtn = document.createElement('button');
+  const radioBtn = document.createElement('div');
+  const fahrenheitBtn = document.createElement('input');
+  const celciusBtn = document.createElement('input');
+  radioBtn.id = 'radioBtn';
+  fahrenheitBtn.setAttribute('type', 'radio');
+  fahrenheitBtn.id = 'fahrenheitBtn';
+  celciusBtn.setAttribute('type', 'radio');
+  celciusBtn.id = 'celciusBtn';
+  fahrenheitBtn.setAttribute('name', 'tempType');
+  const F = document.createElement('span');
+  F.innerHTML = 'F';
+  const C = document.createElement('span');
+  C.innerHTML = 'C';
+  F.id = 'F';
+  C.id = 'C';
+  celciusBtn.setAttribute('name', 'tempType');
+  searchBox.id = 'searchBox';
+  searchBtn.id = 'searchBtn';
+  searchBox.setAttribute('placeholder', 'city');
+  searchBtn.innerHTML = 'check weather';
+  radioBtn.appendChild(fahrenheitBtn);
+  radioBtn.appendChild(F);
+  radioBtn.appendChild(celciusBtn);
+  radioBtn.appendChild(C);
+  weatherInfo.appendChild(searchBox);
+  weatherInfo.appendChild(radioBtn);
+  weatherInfo.append(searchBtn);
+  weatherInfo.appendChild(results);
+  searchBtn.addEventListener('click', getCityName);
+};
+const displayWeatherInfo = (temp, tempMin, tempMax, pressure, humidity) => {
+  const results = document.getElementById('results');
+  results.innerHTML = `<h2>temperature: ${temp.toFixed(2)}</h2>`;
+  results.innerHTML += `<h3>min temperature: ${tempMin.toFixed(2)}</h3>`;
+  results.innerHTML += `<h3>max temperature: ${tempMax.toFixed(2)}</h3>`;
+  results.innerHTML += `<h2>pressure: ${pressure}hPa</h2>`;
+  results.innerHTML += `<h2>humidity: ${humidity}%</h2>`;
+};
 
 export {
-	 displaySearchBox,
-	 displayWeatherInfo
+  displaySearchBox,
+  displayWeatherInfo,
 };
